d3bb598f0188ccccc761e59e036f024ee55ae73e
Binod RD <binodatjaper@gmail.com>
2023-03-15 22:56:13 +0545

    CRUD App completed using Redux Toolkit

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addPost, removePost } from '../store/postSlice';
+import { addPost, removePost, updatePost } from '../store/postSlice';
 
 export default function Post() {
     const dispatch = useDispatch();
-    const datas = useSelector((state) => state.post);
+    const datas = useSelector((state) => state.posts);
+    const [isUpdate, setIsUpdate] = useState(false);
     const [formData, setformData] = useState({
-        title: ''
+        title: '',
+        isActive: false
     });
 
     const handleChange = (e) => {
-        setformData({ ...formData, [e.target.name]: e.target.value });
+        if (e.target.name === 'title') {
+            setformData({ ...formData, [e.target.name]: e.target.value, });
+        } else {
+            setformData({ ...formData, isActive: e.target.checked });
+        }
     }
 
     const handleRemove = (item) => {
@@ -25,36 +31,74 @@ export default function Post() {
     const handleSubmit = (e) => {
         e.preventDefault();
         if (formData.title !== '') {
-            dispatch(addPost(formData));
+            if (isUpdate) {
+                dispatch(updatePost(formData));
+            } else {
+                dispatch(addPost(formData));
+            }
         }
+        setIsUpdate(false);
+        setformData({ ...formData, title: '', isActive: false });
     }
 
     return (
         <div className="container">
-            <div className="row">
-                <div className="col-sm-6">
-                    <h1>Redux Toolkit</h1>
-                    <hr />
-                    <form onSubmit={handleSubmit}>
-                        <input type="text" name="title" onChange={handleChange} className="form-control" />
-                        <br />
-                        <button className='btn btn-primary'>Get Post</button>
-                    </form>
-                    <hr />
-                    <ul>
-                        {datas.map((item, i) => {
-                            return (
-                                <li key={i}>
-                                    <h4>{item.title}</h4>
 
-                                    <button type="button" className="btn btn-success" onClick={() => handleUpdate(item)}>Update</button>
-                                    <button type="button" className="btn btn-danger" onClick={() => handleRemove(item)}>Delete</button>
-                                </li>
-                            );
-                        })}
-                    </ul>
+            <h1>CRUD App with Redux Toolkit</h1>
+            <hr />
+            <form onSubmit={handleSubmit}>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <input type="text" name="title" onChange={handleChange} value={formData.title} className="form-control" placeholder="Enter title" />
+                    </div>
+                    <div className="col-sm-6">
+                        <label htmlFor="isActive">
+                            <input type="checkbox" onChange={handleChange} name="isActive" id="isActive" checked={formData.isActive} />
+                            is Active
+                        </label>
+                    </div>
+                    <div className="col-sm-12">
+                        <button type="submit" className={isUpdate ? 'btn btn-success' : 'btn btn-primary'}>{isUpdate ? 'update Post' : 'Add Post'}</button>
+                    </div>
                 </div>
-            </div>
+            </form>
+
+            <hr />
+            <table className="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>SN</th>
+                        <th>Title</th>
+                        <th>Active</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {datas.map((item, i) => {
+                        return (
+                            <tr key={i}>
+                                <td>{i + 1}</td>
+                                <td>{item.title}</td>
+                                <td>
+                                    <span>
+                                        {
+                                            item.isActive ? 'Active' : 'Deactive'
+                                        }
+                                    </span>
+                                </td>
+                                <td>
+                                    <button type="button" className="btn btn-success" onClick={() => handleUpdate(item)}>
+                                        <i className="fa-solid fa-edit"></i>
+                                    </button>
+                                    <button type="button" className="btn btn-danger" onClick={() => handleRemove(item)}>
+                                        <i className="fa-solid fa-trash"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
         </div>
     );
 }
--- src/store/postSlice.jsx
+++ src/store/postSlice.jsx
@@ -1,24 +1,38 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { v4 as uuidv4 } from 'uuid';
 
+const postsItem = localStorage.getItem('posts') != null ? JSON.parse(localStorage.getItem('posts')) : [];
+const updateLocalStorage = (state) => {
+    localStorage.setItem('posts', JSON.stringify(state.posts.map(item => item)));
+}
+
 const postSlice = createSlice({
     name: 'post',
     initialState: {
-        post: []
+        posts: postsItem
     },
     reducers: {
         addPost(state, actions) {
-            console.log(actions.payload.title);
-            state.post.push({ id: uuidv4(), title: actions.payload.title });
+            state.posts.push({
+                id: uuidv4(),
+                title: actions.payload.title,
+                isActive: actions.payload.isActive
+            });
+            updateLocalStorage(state);
         },
         updatePost(state, actions) {
-
+            state.posts.filter((item, i) => {
+                if (item.id === actions.payload.id) {
+                    state.posts[i] = actions.payload
+                }
+            });
+            updateLocalStorage(state);
         },
         removePost(state, actions) {
-            console.log(actions.payload.id);
-            state.post = state.post.filter(item => item.id !== actions.payload.id);
+            state.posts = state.posts.filter(item => item.id !== actions.payload.id);
+            updateLocalStorage(state);
         }
     }
 });
 export default postSlice;
-export const { getPost, addPost, removePost, filterPost } = postSlice.actions;
+export const { addPost, updatePost, removePost } = postSlice.actions;
